c51a13c75130d68092a35fd1c5b136c4ac25a4f4
Esau <yosoypawn@hotmail.com>
2022-11-01 14:12:50 -0600

    initial

--- app/src/main/java/com/esaudev/componentsgarden/SpinnerFragment.kt
+++ app/src/main/java/com/esaudev/componentsgarden/SpinnerFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import com.esaudev.componentsgarden.databinding.FragmentSpinnerBinding
 
@@ -21,8 +24,34 @@ class SpinnerFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentSpinnerBinding.inflate(inflater, container, false)
 
+        val feelings = createMap()
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.item_dropdown, feelings.map { it.value })
+
+        binding.autoCompleteTextView.apply {
+            setAdapter(arrayAdapter)
+
+            doOnTextChanged { selectedFeeling, _, _, _ ->
+                Toast.makeText(requireContext(), getValueFromMap(feelings, selectedFeeling.toString()).toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.testSpinner.setOnClickListener {
+            Toast.makeText(requireContext(), getValueFromMap(feelings, binding.autoCompleteTextView.text.toString()).toString(), Toast.LENGTH_SHORT).show()
+        }
         
         return binding.root
     }
 
+    private fun createMap(): MutableMap<Int, String> {
+        val feelings = resources.getStringArray(R.array.feelings)
+        val feelingsMap = mutableMapOf<Int, String>()
+        feelings.forEachIndexed { index, feeling ->
+            feelingsMap[index] = feeling
+        }
+        return feelingsMap
+    }
+
+    private fun getValueFromMap(feelingMap: MutableMap<Int, String>, selectedFeeling: String): Int {
+        return feelingMap.map { it.value }.indexOf(selectedFeeling)
+    }
 }
